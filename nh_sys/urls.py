bce40c44c37ec939630e9e337cb2847c8f0926e2
eddym <edwinmuthee11@gmail.com>
2022-10-29 16:07:33 +0300

    feat(repair): repair crud endpoint
    + add drf schema
    + add model
    + add serializer

--- nh_sys/urls.py
+++ nh_sys/urls.py
@@ -13,13 +13,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import (
     include,
     path,
 )
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+admin.site.site_header = "NHS Administration"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.urls")),
 ]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="NHS API",
+        default_version="v1",
+        description="NHS API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path(
+            "docs/",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+        path(
+            "docs/swagger/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+    ]
