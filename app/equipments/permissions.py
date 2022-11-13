d8b949321b010746ff6c3d205c861678b325e0d6
eddym <edwinmuthee11@gmail.com>
2022-11-13 13:09:17 +0300

    Allow user updates in permission

--- app/equipments/permissions.py
+++ app/equipments/permissions.py
@@ -21,6 +21,6 @@ class IsHodorAdmin(BasePermission):
         ):
             return True
 
-        if obj.user == request.user or request.user.is_superuser:
+        if request.user.is_superuser:
             return True
         return False
