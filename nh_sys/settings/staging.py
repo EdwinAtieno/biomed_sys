396853cb0a45294c6a2d7d8e3b247c9e4b9f7ea3
eddym <edwinmuthee11@gmail.com>
2023-02-19 22:40:29 +0300

    cross origins updates

--- nh_sys/settings/staging.py
+++ nh_sys/settings/staging.py
@@ -21,5 +21,6 @@ CORS_ALLOWED_ORIGIN_REGEXES += [
     r"^(http?:\/\/)?((localhost)|(127\.0\.0\.1)):3\d{3}",
     r"^(http?:\/\/)?((localhost)|(127\.0\.0\.1)):5\d{3}",
     r"^https:\/\/nh-sys-*",
+    r"^https:\/\/biomedsys-*",
     r"(^|^[^:]+:\/\/|[^\.]+\.)nh-sys\.co\.ke",
 ]
