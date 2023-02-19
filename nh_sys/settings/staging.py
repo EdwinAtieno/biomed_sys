03961701ce4b59a942a573bfa2ebac043cb2dad1
eddym <edwinmuthee11@gmail.com>
2023-02-19 22:22:39 +0300

    staging

--- nh_sys/settings/staging.py
+++ nh_sys/settings/staging.py
@@ -14,12 +14,12 @@ ALLOWED_HOSTS += [
 CORS_ALLOWED_ORIGINS += [
     "https://nh_sys-fend-pwa.herokuapp.com",
     "https://nh_sys-proxy.herokuapp.com",
-    "https://nh_sys.netlify.app",
+    "https://nh-sys.netlify.app",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES += [
     r"^(http?:\/\/)?((localhost)|(127\.0\.0\.1)):3\d{3}",
     r"^(http?:\/\/)?((localhost)|(127\.0\.0\.1)):5\d{3}",
-    r"^https:\/\/nh_sys-*",
-    r"(^|^[^:]+:\/\/|[^\.]+\.)nh_sys\.co\.ke",
+    r"^https:\/\/nh-sys-*",
+    r"(^|^[^:]+:\/\/|[^\.]+\.)nh-sys\.co\.ke",
 ]
